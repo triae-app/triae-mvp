6b3ec2f6e632822c651bb3d7c30f57e39d973117
Cristian Freitas <crismdf2003@gmail.com>
2025-11-10 21:16:28 -0300

    remove (api): delete initial hello world route

--- api/src/http/routes/hello-world.ts
+++
@@ -1,7 +0,0 @@
-import type { FastifyInstance } from 'fastify'
-
-export async function helloWorld(app: FastifyInstance) {
-	app.get('/', async () => {
-		return 'Hello World!'
-	})
-}
--- api/src/http/routes/index.ts
+++ api/src/http/routes/index.ts
@@ -1,10 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import { betterAuth } from './better-auth'
-import { helloWorld } from './hello-world'
 import { leadsRoutes } from './leads-routes'
 
 export async function httpRoutes(app: FastifyInstance) {
-	app.register(helloWorld)
 	app.register(betterAuth)
 	app.register(leadsRoutes)
 }
