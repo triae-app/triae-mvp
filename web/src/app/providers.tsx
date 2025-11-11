4021317c603278c6b66ecf52b158cc758047aa57
Cristian Freitas <crismdf2003@gmail.com>
2025-11-10 23:07:56 -0300

    feat(web): add next-themes

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -1,10 +1,19 @@
 'use client'
 
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'next-themes'
 import { queryClient } from '@/lib/query-client'
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="dark"
+				disableTransitionOnChange
+			>
+				{children}
+			</ThemeProvider>
+		</QueryClientProvider>
 	)
 }
